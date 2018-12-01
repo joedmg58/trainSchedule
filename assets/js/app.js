757ee1643b3623fb1cb39dc2b3c2b5206225d592
Joed Machado <joedmg58@gmail.com>
2018-11-30 22:25:52 -0500

    some fixed and modifications added

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,26 +12,24 @@ trainDestinationInput = '#destination'; // id of the destination input text
 trainFirstTimeInput = '#firstTrain'; // id of the first time input
 trainFrequencyInput = '#frequency'; // id of the frequency input
 
+
 //It will start when the document will finish to load
 $(document).ready( function() {
 
     // Initialize Firebase
-    var databaseName = 'trainsystemhw'; //we need to fill this
-
     var config = {
-        apiKey: "AIzaSyBdoJiMy_PFktLmrveH0A2nbbj8wtvur_o", //we need to fill this
-        authDomain: databaseName + ".firebaseapp.com",
-        databaseURL: "https://" + databaseName + ".firebaseio.com",
-        projectId: databaseName,
-        storageBucket: "",
-        messagingSenderId: "695028065097" //we need to fill this 
+        apiKey: "AIzaSyDX0UgJb4JP6UIfW9wl2VQtb7lJ18OgpCY",
+        authDomain: "trainschedule-991ea.firebaseapp.com",
+        databaseURL: "https://trainschedule-991ea.firebaseio.com",
+        projectId: "trainschedule-991ea",
+        storageBucket: "trainschedule-991ea.appspot.com",
+        messagingSenderId: "769362498777"
     };
-
+    
     firebase.initializeApp(config);
 
     //Reference to the train schedule database
     var fbTrainSchedule = firebase.database();
-    console.log( fbTrainSchedule );
 
     //Registering onclick event for adding train
     $( addButtonId ).click( addTrain );
@@ -42,7 +40,7 @@ $(document).ready( function() {
     
         var trainName = $( trainNameInput ).val().trim();
         var trainDestination = $( trainDestinationInput ).val().trim();
-        var trainFirstTime = moment( $( trainFirstTimeInput ).val().trim(), 'MM/DD/YYYY' ).format('HH:mm');
+        var trainFirstTime = $( trainFirstTimeInput ).val().trim();
         var trainFrequency = $( trainFrequencyInput ).val().trim();
     
         fbTrainSchedule.ref().push( {
@@ -66,14 +64,15 @@ $(document).ready( function() {
 
     //Its triggers when a new child (element) to the database is added
     fbTrainSchedule.ref().on( "child_added", function( snapshot ) {
+
         //get values from snapshot
         var name = snapshot.val().name;
         var dest = snapshot.val().destination;
         var first = snapshot.val().first;
         var freq = parseInt( snapshot.val().frequency );
+
         //calculated values
         var departureTimes = Math.abs( Math.ceil( moment( first, 'HH:mm').diff( moment(), 'minutes' ) / freq ) );
-        console.log(departureTimes);
         var na = moment( first, 'HH:mm').add( ((departureTimes+1) * freq), 'minutes' );
         var nextArr = moment( na, 'HH:mm' ).format('hh:mm a');
         var minAway = moment( na, 'HH:mm' ).diff( moment(), 'minutes') + 1;
@@ -84,7 +83,9 @@ $(document).ready( function() {
         $('<td>').text( dest ).appendTo( row );
         $('<td>').text( freq ).appendTo( row );
         $('<td>').text( nextArr ).appendTo( row );
-        $('<td>').text( minAway ).appendTo( row );
+        $('<td id="col-icon">').html( '<span>' + minAway + '</span> <i class="fas fa-trash"></i>' )
+                               .appendTo( row );
+        $('.fas').css({"visibility":"visible", "float":"right", "cursor":"pointer"});
 
     } );
 
